6da07099308d2c97c79353452325acb42a777728
Ruhiyahfw <faradishi@gmail.com>
2022-03-23 16:54:32 +0700

    load+save

--- event-listener.js
+++ event-listener.js
@@ -180,3 +180,67 @@ function handleReset() {
         }
     }
 }
+
+/* save file */
+function saveFile() {
+    // get filename
+    var filename = document.getElementById("filename").value;
+    if (filename == "") {
+      filename = "webgl project";
+    }
+  
+    // collect project's variables
+    var objToExport = [];
+    objToExport.push(arrTransformation);
+  
+    // stringify objToExport
+    var data = JSON.stringify(objToExport);
+  
+    // download data
+    download(filename + ".json", data);
+  
+    // show success info
+    alert("Save file success");
+  }
+
+  var download = function (filename, text) {
+    var element = document.createElement("a");
+    element.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+    );
+    element.setAttribute("download", filename);
+  
+    element.style.display = "none";
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+  };
+
+/* load file */
+function loadFile() {
+    // get file chosen
+    var file = document.getElementById("import_file").files[0];
+    if (!file) {
+      alert("Choose a json file first");
+      return;
+    }
+  
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      console.log("file imported");
+      arrObjects = JSON.parse(e.target.result);
+      LoadData(arrObjects);
+    };
+  
+    reader.readAsText(file);
+  }
+  
+  function LoadData(arrObjects) {
+    // reset
+    handleReset()
+    // save new global variables
+    arrTransformation = arrObjects[0];
+  }
